2ca555ecb259878d7c91f1622ec63e4448d34721
sam <sam@sam-PC>
2017-05-07 01:10:34 +0800

    修改了 fcef=2

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -10,7 +10,7 @@ public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		int fcf=2;
+		int fcef=2;		
 		System.out.println(1);
 	}
 	
