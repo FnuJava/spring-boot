0e271cd27aa07293647050de5321f9c92a3f2452
FnuJava <375723803@qq.com>
2017-05-07 10:43:49 +0800

    abc1提交
    abc1提交

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -10,7 +10,7 @@ public class Application {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		int fddda=2;
+		int abc1=2;
 		System.out.println(1);
 	}
 	
